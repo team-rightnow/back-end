8e678ac22ed5575a63a75a39746f52c493cb7976
dogmania <hj296914@gmail.com>
2025-01-02 17:36:34 +0900

    feat: 일기 조회 JPA 함수 구현

--- src/main/java/com/sidenow/team/rightnow/diary/repository/DiaryRepository.java
+++ src/main/java/com/sidenow/team/rightnow/diary/repository/DiaryRepository.java
@@ -2,6 +2,12 @@ package com.sidenow.team.rightnow.diary.repository;
 
 import com.sidenow.team.rightnow.diary.entity.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
+    List<Diary> findByUserIdAndDeletedFalse(Long userId);
 }
