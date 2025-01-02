0d64c2243a7448a8bbf540bc95754d16b08c9c65
dogmania <hj296914@gmail.com>
2025-01-02 17:36:58 +0900

    BaseEntity 프로퍼티 추가

--- src/main/java/com/sidenow/team/rightnow/global/BaseEntity.java
+++ src/main/java/com/sidenow/team/rightnow/global/BaseEntity.java
@@ -1,4 +1,24 @@
 package com.sidenow.team.rightnow.global;
 
-public class BaseEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdDate;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedDate;
 }
