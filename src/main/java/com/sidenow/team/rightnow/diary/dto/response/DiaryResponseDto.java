b7285eb7a7301d1be96383938b30652e34938316
dogmania <hj296914@gmail.com>
2025-01-02 17:47:31 +0900

    다이어리 응답 Dto 수정

--- src/main/java/com/sidenow/team/rightnow/diary/dto/response/DiaryResponseDto.java
+++ src/main/java/com/sidenow/team/rightnow/diary/dto/response/DiaryResponseDto.java
@@ -1,11 +1,16 @@
 package com.sidenow.team.rightnow.diary.dto.response;
 
+import com.sidenow.team.rightnow.diary.entity.Diary;
 import com.sidenow.team.rightnow.diary.entity.InnerPage;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class DiaryResponseDto {
     private Long id;
     private String title;
@@ -13,4 +18,17 @@ public class DiaryResponseDto {
     private Integer temperature;
     private InnerPage innerPage;
     private Boolean deleted;
+    private LocalDateTime createdDate;
+
+    public static DiaryResponseDto from(Diary diary) {
+        return DiaryResponseDto.builder()
+                .id(diary.getId())
+                .title(diary.getTitle())
+                .content(diary.getContent())
+                .temperature(diary.getTemperature())
+                .innerPage(diary.getInnerPage())
+                .deleted(diary.getDeleted())
+                .createdDate(diary.getCreatedDate())
+                .build();
+    }
 }
